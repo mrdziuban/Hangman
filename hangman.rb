ed05b000f9c97b05894f2883cf2fb243904a4a1b
mrdziuban <mrdziuban@gmail.com>
2013-06-21 18:38:19 -0700

    Completed evaluator methods

--- hangman.rb
+++ hangman.rb
@@ -5,6 +5,19 @@ end
 class HumanPlayer
 	attr_accessor :word, :word_state
 
+	# Guess is single letter
+	# Ask if letter is in word
+	# Return locations or "no" if not included
+	def evaluate_guess(guess)
+		print "Is #{guess} included in your word?"
+		answer = gets.chomp
+		if answer == "no"
+			return "no"
+		elsif answer == "yes"
+			print "At what indices? (format '1 2 3') "
+		letter_locations = gets.chomp.split(" ").map {|i| i.to_i}
+		letter_locations
+	end
 
 	# Check winning guess when human is hangman (full word guesses)
 	def winning_guess?(guess)
@@ -23,6 +36,7 @@ class ComputerPlayer
 
 	# Guess is single letter
 	# Check if letter is in word
+	# Return locations or "no" if not included
 	def evaluate_guess(guess)
 		letters = self.word.split(//)
 		letter_locations = []
@@ -30,6 +44,11 @@ class ComputerPlayer
 			if letter == guess
 				letter_locations << index
 			end
+		end
+		if letter_locations.length == 0
+			return "no"
+		else
+			return letter_locations
 		end
 	end
 
